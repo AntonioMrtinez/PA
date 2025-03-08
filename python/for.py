e1f0e388d13900805a666be6802b2fc14d60174b
AntonioMrtinez <zs23013950@estudiantes.uv.mx>
2025-03-07 19:03:44 -0600

    FOR and WHILE
    son dos pequeñas pruebas de ford y while, en las que se hace un contador, hasta un momento definido o  hasta que cierta condición se cumpla

---
+++ python/for.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  7 19:00:15 2025
+
+@author: LENOVO
+"""
+
+j = 0   
+i = 0  
+
+while i < 10: 
+    j += 1 
+    i += 1  
+
+print(j) 
---
+++ python/while.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  7 18:54:06 2025
+
+@author: LENOVO
+"""
+
+i = 0
+
+for _ in range(10):
+    i += 1
+
+print(i)
